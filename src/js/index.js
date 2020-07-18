bf27d9ed8b0b5a6d02b08dfc5b36042d9396de4b
Michal Duszak <drduszak@gmail.com>
2020-07-18 09:51:15 +0200

    initial

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,36 @@
 import '../scss/main.scss';
 
 // uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+import {registerSW} from './pwa.js';
+registerSW();
 
-/* place your code below */
 
-console.log('HELLO 🚀')
+const glassCounter = document.querySelector(".hydrapp__glass-counter--js");
+const incrementCounter = document.querySelector(".hydrapp__buttons-increase--js");
+const decreaseCounter = document.querySelector(".hydrapp__buttons-decrease--js");
+var counter = 0;
+var key = new Date().toISOString().slice(0,10);
+function updateTheDate() {
+    if (Object.keys(window.localStorage).includes(key)) {
+        counter = localStorage.getItem(key);
+        glassCounter.innerHTML = counter;
+    } else {
+        localStorage.setItem(key, 0);
+    }
+};
+function addGlass() {
+    counter++;
+    glassCounter.innerHTML = counter;
+    localStorage.setItem(key, counter);
+};
+function removeGlass() {
+    if(counter > 0) {
+        counter--;
+        glassCounter.innerHTML = counter;
+        localStorage.setItem(key, counter);
+    }; 
+};
+updateTheDate();
+incrementCounter.addEventListener('click', addGlass);
+decreaseCounter.addEventListener('click', removeGlass);
+
